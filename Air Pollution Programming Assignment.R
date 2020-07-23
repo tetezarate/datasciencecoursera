5e8c1be29d384dba1a842b1c42073d5e7461c260
Camilo Zarate <c.zarate10@uniandes.edu.co>
2020-07-22 19:40:20 -0500

    Update of the code to calculate the mean for ALL the files

--- Air Pollution Programming Assignment.R
+++ Air Pollution Programming Assignment.R
@@ -1,4 +1,27 @@
+#install.packages("readr")
+#install.packages("pastecs")
 pollutantmean <- function(directory, pollutant, id=1:332) {
-  
-  
+  folderPath <- file.path(getwd(),directory)
+  fileList <- list.files(folderPath)
+  for (file in fileList) {
+    # if the merged dataset doesn't exist, create it
+    if (!exists("dataset")){
+      dataset <- read.csv(file.path(folderPath,file), header=TRUE, sep=",")
+    } else{
+      temp_dataset <- read.csv(file.path(folderPath,file), header=TRUE, sep=",")
+      dataset<-rbind(dataset, temp_dataset)
+      rm(temp_dataset)
+    }
+  }
+  vectorToAnalyze <- dataset[[pollutant]]
+  mean(vectorToAnalyze, na.rm = TRUE)
 }
+
+
+
+
+
+
+
+
+
